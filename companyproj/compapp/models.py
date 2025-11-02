26a6d2bd23eeed4f7b6db99ca99048878633b4c0
modhialotaibi <modhi.alotaibi@cba.ku.edu.kw>
2025-11-02 14:34:30 +0300

    changes

---
+++ companyproj/compapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    position = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=8)
+    joined_on = models.DateField()
+    
+    def __str__(self):
+        return f"{self.name} - {self.position}"
---
+++ companyproj/compapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Employee
+# Create your views here.
+def employee_view(request):
+    employees = Employee.objects.all().order_by('name')
+    return render(request, 'employee_list.html',
+                  {'employees': employees})
